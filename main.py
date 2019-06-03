2bc5301a8c097b10b3ab5fc062255767b669b789
DevHyung <khuphj@gmail.com>
2019-06-03 21:17:24 +0900

    Update parsing하는부분 + 페이징이동

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from time import sleep
 ''' CONFIG AREA '''
+BASE_URL = 'https://search.naver.com/search.naver'
 DRIVER_PATH = "./chromedriver"
 SEARCH_KEYWORD = "암"
-START_DATE = "2009.01.01"
+START_DATE = "2018.12.15"
 END_DATE =  "2018.12.31"
 JOURNAL_NAME = "조선일보"
+PAGE_LIMIT = 5
 
 
 if __name__ == "__main__":
@@ -54,3 +56,24 @@ if __name__ == "__main__":
     sleep(1)
 
 # parsing
+    outF = open("{}_{}-{}_URL.txt".format(JOURNAL_NAME,START_DATE.replace('.',''),END_DATE.replace('.','')),'w')
+    for _ in range(PAGE_LIMIT):
+        bs = BeautifulSoup(driver.page_source, 'lxml')
+        # extract URL
+        ul = bs.find('ul',class_='type01')
+        lis = ul.find_all('li')
+        for li in lis:
+            a = li.find('a', class_=' _sp_each_title')
+            link = a['href'].strip()
+            outF.write(link+'\n')
+
+        # Paging
+        pageLastA = bs.find('div', class_='paging').find_all('a')[-1]
+        if pageLastA.get_text().strip() == "다음페이지":
+            nextLink = BASE_URL+pageLastA['href']
+            driver.get(nextLink)
+        else:
+            # Last page
+            break
+        sleep(2)
+    outF.close()
